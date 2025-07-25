a9eafb5425f1d7dacd6864a485754dd13a7d3e92
Willian Cueva <69866069+Willian-Cueva@users.noreply.github.com>
2025-07-25 10:07:14 -0500

    feat: add FTP deployment script and configuration for automated builds

---
+++ deploy.js
@@ -0,0 +1,35 @@
+const ftp = require("basic-ftp")
+const path = require("path")
+const dotenv = require("dotenv")
+
+dotenv.config() // Cargar variables de entorno
+
+async function deploy() {
+    const client = new ftp.Client()
+    client.ftp.verbose = true
+
+    try {
+        await client.access({
+            host: process.env.FTP_HOST,
+            user: process.env.FTP_USER,
+            password: process.env.FTP_PASSWORD,
+            secure: false
+        })
+
+        const localPath = path.join(__dirname, "dist") // o "dist"
+        // const remotePath = process.env.FTP_REMOTE_PATH || "/public_html"
+
+        // await client.ensureDir(remotePath)
+        await client.clearWorkingDir()
+        await client.uploadFromDir(localPath)
+
+        console.log("🚀 ¡Despliegue completado!")
+
+    } catch (err) {
+        console.error("❌ Error en el despliegue:", err)
+    }
+
+    client.close()
+}
+
+deploy()
